440414ffedb74cac322735b3d15bd66684e38508
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-02-03 14:03:06 -0500

    Add page to view all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.page(params[:page])
-    @lists = List.all
+    @posts = find_posts
+    @lists = List.pluck(:category, :name)
+  end
+
+  private 
+
+  def find_posts
+    posts = Post.page(params[:page])
+    posts = posts.by_social_media(params[:social_media]) if params[:social_media].present?
+    posts = posts.by_postable_type(params[:type]) if params[:type].present?
+    posts = posts.by_date_range(params[:date][:from], params[:date][:to]) if params[:date].present? && params[:date][:from].present? &&  params[:date][:to].present?
+    posts
   end
 end
