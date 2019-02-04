0718a5a94139443cc4f8d855515cb03e84a4e762
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-02-04 13:12:30 -0500

    add controller spec

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  describe 'GET #index' do
+    it "renders :index template" do
+      get :index 
+      expect(response).to render_template(:index)
+    end
 
+    it "asssigns posts" do
+      post = create(:governor_post)
+      get :index 
+      expect(assigns(:posts)).to match_array([post])
+    end
+
+    it "assigns lists" do
+      list = create(:list)
+      get :index 
+      expect(assigns(:lists)).to include(include(list.name, list.category))
+    end
+  end
 end
